ce0b670b19fde53ea5fc9b44567794577747ab30
Alexey Lizurchik <al.lizurchik@gmail.com>
2015-02-13 01:15:56 +0300

    init alpha version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# onliner-firefox
-Plugin to switch prices USD or BLR
+# About
+Firefox add-on to switch prices to USD or BLR at onliner.by
+
+# Status
+v1.0.0-alpha (unstable)
+Find compiled add-on here: `build/onliner-price-switcher.xpi`
---
+++ data/content.js
@@ -0,0 +1,16 @@
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
+	var $priceSelector = $j('.pprice, .product-aside__price--primary, .b-offers-desc__info-sub > a');
+  var currency = parseInt($j('#currency-informer > a > ._u').text().replace(/[\s\$]+/g, ''), 10);
+
+	$priceSelector.each(function() {
+		var originalPriceStr = $j(this).text();
+		var priceWithoutSpaces = originalPriceStr.trim().replace(/\s/g, '');
+		var prices = priceWithoutSpaces.split('-');
+		prices = prices.map(function(el) {
+			return Math.floor(el.match(/[0-9]*/g, '')[0] / currency);
+		});
+		$j(this).text('~ ' + prices.join(' - ') + ' $');
+	});
+});
